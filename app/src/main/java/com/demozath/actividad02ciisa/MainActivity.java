004b76e39ad835acfca3c2d04c6557243b28f07d
Demozath <abeljsmz@gmail.com>
2023-06-09 01:07:17 -0400

    Se inicia repositorio actividad02 android studio

---
+++ app/src/main/java/com/demozath/actividad02ciisa/MainActivity.java
@@ -0,0 +1,49 @@
+package com.demozath.actividad02ciisa;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String usuario = "CIISA";
+    String password = "4321";
+    MediaPlayer sonidoError;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnIngresar(View view){
+        EditText etNombre = (EditText) findViewById(R.id.etNombre);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        String Nombreusuario = etNombre.getText().toString();
+        String PasswordUsuario = etPassword.getText().toString();
+
+        if (Nombreusuario.equals(this.usuario) && (PasswordUsuario.equals(this.password))){
+            Toast toast = Toast.makeText(getApplicationContext(),"Bienvenido !",Toast.LENGTH_LONG);
+            toast.show();
+            Intent intent = new Intent(MainActivity.this,LobbyActivity.class);
+            startActivity(intent);
+
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(),"Usuario Incorrecto :C !",Toast.LENGTH_LONG);
+            toast.show();
+            sonidoError = MediaPlayer.create(this,R.raw.cuack);
+            sonidoError.start();
+            Vibrator vibracionError = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+            vibracionError.vibrate(1000);
+        }
+
+    }
+}
